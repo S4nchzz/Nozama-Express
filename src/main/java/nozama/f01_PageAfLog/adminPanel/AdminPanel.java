7072cd61f99e3d607a5a7fdfab7e81d7bfa4b97c
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2024-04-06 16:46:32 +0200

    Se ha añadido el boton gestion de usuario en el admin panel con animacion cuando se pasa el raton por encima, tambien se añadio en metodo onClick del pane del boton

--- src/main/java/nozama/f01_PageAfLog/adminPanel/AdminPanel.java
+++ src/main/java/nozama/f01_PageAfLog/adminPanel/AdminPanel.java
@@ -1,5 +1,10 @@
 package nozama.f01_PageAfLog.adminPanel;
 
+import javafx.fxml.FXML;
+
 public class AdminPanel {
-    
+    @FXML
+    private void handleGestionUsuarios () {
+        System.out.println("ASd");
+    }
 }
