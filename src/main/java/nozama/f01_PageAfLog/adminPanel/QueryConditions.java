adce35169d6f7ea3005ee3f9576bf6bf8432a7fe
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2024-04-07 23:46:46 +0200

    Añadidas nuevas condiciones | Funcionalidad no dicha en el anterior : El metood injectCustomQuery retorna un objeto de tipo Object, despues en el metodo sendQueryAdmin, el cual recoge dicho objeto lo analiza para ver si el retorno es un ResultSet o un String, ya que ambos heredad de Object, dependiendo de la condicion del if hecha con instanceof (if ob instanceof String) {} else {} si es String se guardara el contenido haciendo un casting de Object a String String a = (String)obj, y ocurre lo mismo de la otra forma con el ResultSet, si al final el objeto es de tipo ResultSet operara para cambiar el contenido de la tabla, en cambio si es String querra decir que en la consulta hubo un error y este lo mostrara en pantalla, quitando la cadena de caracteres (con=999) que aparece al principio del error

--- src/main/java/nozama/f01_PageAfLog/adminPanel/QueryConditions.java
+++ src/main/java/nozama/f01_PageAfLog/adminPanel/QueryConditions.java
@@ -1,5 +1,7 @@
 package nozama.f01_PageAfLog.adminPanel;
 
+import javax.swing.JOptionPane;
+
 public class QueryConditions {
     private final String query;
 
@@ -8,11 +10,36 @@ public class QueryConditions {
     }
 
     protected boolean conditions () {
-        if (deleteFromCondition() && alterTableCondition() && dropDatabaseCondition()) {
+        if (deleteFromCondition() && alterTableCondition() && dropDatabaseCondition() && dropTableCondition() && createProcedure() && select()) {
             return true;
         }
 
         return false;
+    }
+
+    private boolean select () {
+        if (!this.query.contains("SELECT *")) {
+            JOptionPane.showMessageDialog(null, "Funcion de seleccion de campo unico aun no implementada");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean createProcedure () {
+        if (this.query.contains("CREATE PROCEDURE") || this.query.contains("CREATE FUNCTION")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean dropTableCondition () {
+        if (this.query.contains("DROP TABLE")) {
+            return false;
+        }
+
+        return true;
     }
 
     private boolean dropDatabaseCondition () {
